9b8c07ac9cd68162e463348997e79840837e21f1
adarsh <adarsh.ADARTZ66@gmail.com>
2023-10-23 21:44:54 +0530

    first commit

---
+++ src/about/subheadingsData.js
@@ -0,0 +1,65 @@
+const subheadingsData = {
+    1: [
+        {
+            title: "About Me",
+            content: (
+                <>
+                  <p>
+                   I'm Adarsh Prasad, a dedicated Frontend Developer and an aspiring BSc Computer Science student. 
+                   My journey is characterized by a relentless pursuit of perfection in web design and development.
+                   where technology meets creativity, and I'm here to make your digital dreams come to life.
+                  </p>
+                </>
+            ),
+        },
+    ],
+    2: [
+        {
+            title: "IT Executive at Ooma Cyber Solution",
+            content: (
+                <>
+                  <p>
+                  I worked at Ooma Cyber Solutions as an IT Executive. 
+                  In this role, I was responsible for overseeing the company's information technology infrastructure and ensuring its optimal functionality and security.
+                  </p>
+                </>
+            ),
+        },
+        {
+            title: "Working as a Freelancer for System Troubleshooting",
+            content: (
+                <>
+                  <p>
+                  I work as a Freelancer,offering quick and effective solutions for IT issues in both individual and business settings.
+                  </p>
+                </>
+            ),
+        },
+    ],
+    3: [
+        {
+            title: "Front End Developer",
+            content: (
+                <>
+                  <p>
+                  I'm a front-end developer, specializing in creating user-friendly web interfaces. 
+                  I use HTML, CSS, and JavaScript to design visually appealing and responsive websites. 
+                  My focus is on optimizing the user experience and ensuring effective interaction with digital platforms.
+                  </p>
+                </>
+            ),
+        },
+        {
+            title: "Web Designer",
+            content: (
+                <>
+                  <p>
+                  
+                  </p>
+                </>
+            ),
+        },
+    ],
+};
+
+export default subheadingsData;
